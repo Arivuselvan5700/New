3da097e8f241677abaad5cfea74ddfc7acb06425
Arivuselvan <arivuselvan96@gmail.com>
2019-12-17 13:21:07 +0530

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+var a = [1, 3, 5, 6];
+var b = [6, 7, 8, 9];
+var c = [];
+for (let i = 0; i < a.length; i++) {
+  let j = 0;
+  while (j < b.length) {
+    a[i] > b[i];
+    c.push();
+    j++;
+  }
+}
+console.log(c);
